d8218948bd7b90fdf41dfdb8a043a3b1af6ab739
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2024-02-27 14:10:54 +0100

    feat(args) Parse args and exit on error in Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main (main) where
 
-import Lib
+import System.Exit (exitWith, exitSuccess, ExitCode(ExitFailure))
+import System.IO (hPutStrLn, stderr)
+import Conf (getConf, Conf(Conf, rule))
+import Lib (wolfram)
 
 main :: IO ()
-main = someFunc
+main = do
+    conf <- getConf
+    case conf of
+        Nothing -> hPutStrLn stderr "Invalid arguments" >> exitWith (ExitFailure 84)
+        Just (Conf {rule=Nothing}) -> hPutStrLn stderr "No rule specified, use `--rule <rule number>'" >> exitWith (ExitFailure 84)
+        Just config -> wolfram config
+    exitSuccess
