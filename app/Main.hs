a82423c487221cf323a5b03f97889a9cc547c0b6
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2024-02-29 03:19:49 +0100

    feat(rule) Add a proper Rule record suitable for all the possible rules

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import System.Exit (exitWith, exitSuccess, ExitCode(ExitFailure))
 import System.IO (hPutStrLn, stderr)
+
 import Conf (getConf, Conf(Conf, rule))
 import Lib (wolfram)
 
@@ -10,6 +11,6 @@ main = do
     conf <- getConf
     case conf of
         Nothing -> hPutStrLn stderr "Invalid arguments" >> exitWith (ExitFailure 84)
-        Just (Conf {rule=Nothing}) -> hPutStrLn stderr "No rule specified, use `--rule <rule number>'" >> exitWith (ExitFailure 84)
+        Just Conf {rule=Nothing} -> hPutStrLn stderr "No rule specified, use `--rule <rule number>'" >> exitWith (ExitFailure 84)
         Just config -> wolfram config
     exitSuccess
---
+++ src/Rule.hs
@@ -0,0 +1,26 @@
+module Rule (Rule(Rule, r111, r110, r101, r100, r011, r010, r001, r000), maybeRule) where
+
+import Data.Bits ((.&.), complement)
+
+data Rule = Rule {
+    r111 :: Bool,
+    r110 :: Bool,
+    r101 :: Bool,
+    r100 :: Bool,
+    r011 :: Bool,
+    r010 :: Bool,
+    r001 :: Bool,
+    r000 :: Bool
+} deriving Show
+
+maybeRule :: Int -> Maybe Rule
+maybeRule x | (x .&. complement 0xff) /= 0 = Nothing
+            | otherwise = Just (Rule
+                ((x .&. 0x80) /= 0)
+                ((x .&. 0x40) /= 0)
+                ((x .&. 0x20) /= 0)
+                ((x .&. 0x10) /= 0)
+                ((x .&. 0x08) /= 0)
+                ((x .&. 0x04) /= 0)
+                ((x .&. 0x02) /= 0)
+                ((x .&. 0x01) /= 0))
