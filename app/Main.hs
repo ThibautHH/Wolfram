e20a867271f7f0ab4fde9da747ecf3137591a81a
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2024-02-29 03:57:21 +0100

    fix(main) Actually evaluate the result obtained with the config

--- app/Main.hs
+++ app/Main.hs
@@ -9,8 +9,8 @@ import Lib (wolfram)
 main :: IO ()
 main = do
     conf <- getConf
-    case conf of
+    ret <- case conf of
         Nothing -> hPutStrLn stderr "Invalid arguments" >> exitWith (ExitFailure 84)
         Just Conf {rule=Nothing} -> hPutStrLn stderr "No rule specified, use `--rule <rule number>'" >> exitWith (ExitFailure 84)
         Just config -> wolfram config
-    exitSuccess
+    ret >> exitSuccess
